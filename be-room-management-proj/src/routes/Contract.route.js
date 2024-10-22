a78347cd9f57c5d9bb56e254e2543c69be198025
dohuuphuc2703 <dohuuphuc2002@gmail.com>
2024-10-22 15:59:46 +0700

    API Contract

---
+++ be-room-management-proj/src/routes/Contract.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+const ContractController = require("../app/controllers/Contract.controller")
+
+router.post("/create", ContractController.createContract);
+router.get("/:id", ContractController.getContract);
+router.delete("/delete", ContractController.deleteContract);
+router.post("/mine", ContractController.getMyContracts);
