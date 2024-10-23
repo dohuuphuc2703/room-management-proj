99edd8b64b921d851b9442347d0989698ccd7a9c
dohuuphuc2703 <dohuuphuc2002@gmail.com>
2024-10-23 14:17:00 +0700

    update 23/10

--- be-room-management-proj/src/app/models/MaintenanceRequest.model.js
+++ be-room-management-proj/src/app/models/MaintenanceRequest.model.js
@@ -3,23 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MaintenanceRequestSchema = new Schema({
-    tenant: { 
+    contract: { 
         type: Schema.Types.ObjectId, 
-        ref: "Tenant",
-        unique: true,
-        required: true,
-    },
-
-    landlord: { 
-        type: Schema.Types.ObjectId, 
-        ref: "Landlord",
-        unique: true,
-        required: true,
-    },
-
-    room: { 
-        type: Schema.Types.ObjectId, 
-        ref: "Room",
+        ref: "Contract",
         unique: true,
         required: true,
     },
--- be-room-management-proj/src/routes/Review.route.js
+++ be-room-management-proj/src/routes/Review.route.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const ReviewController = require("../app/controllers/Review.controller")
 const { roleVerify } = require("../app/middlewares/roleMiddleware");
+const { verifyJwt } = require("../app/middlewares/jwtMiddleware");
 
 router.get("/room/:roomId", ReviewController.getReviewsByRoom);
-router.post("/create", ReviewController.createReview);
+router.post("/create",verifyJwt, ReviewController.createReview);
 router.post("/update/:reviewId", ReviewController.updateReview);
 router.delete("/delete/:reviewId", ReviewController.deleteReview);
 
