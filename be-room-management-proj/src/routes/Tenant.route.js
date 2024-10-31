c946fa39f069f776b1879d70898f0d9aaa8ab1bc
dohuuphuc2703 <dohuuphuc2002@gmail.com>
2024-10-31 17:13:24 +0700

    update_31/10

--- be-room-management-proj/src/routes/Tenant.route.js
+++ be-room-management-proj/src/routes/Tenant.route.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const TenantController = require("../app/controllers/Tenant.controller")
 const { roleVerify } = require("../app/middlewares/roleMiddleware");
+const { verifyJwt } = require("../app/middlewares/jwtMiddleware");
 
 router.post("/info/", TenantController.updateInfo);
-router.get("/info/", TenantController.getInfo);
+router.get("/info/",verifyJwt, TenantController.getInfo);
 router.get("/all-saved-rooms/", TenantController.getAllSavedRooms);
 router.post("/save-room/", TenantController.saveRoom);
 
