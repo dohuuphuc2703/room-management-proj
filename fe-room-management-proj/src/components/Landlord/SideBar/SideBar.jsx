730c2cd4c3fabe65293262d76679fc97ca86d5df
dohuuphuc2703 <dohuuphuc2002@gmail.com>
2024-11-08 16:01:25 +0700

    landlordView1

--- fe-room-management-proj/src/components/Landlord/SideBar/SideBar.jsx
+++ fe-room-management-proj/src/components/Landlord/SideBar/SideBar.jsx
@@ -20,7 +20,7 @@ function SideBar() {
   };
 
   return (
-    <div className={styles.container}>
+ 
       <div className={`${styles.sidebar} ${collapsed ? styles.close : ''}`}>
         <div className={styles.logoDetails}>
           <AppstoreOutlined />
@@ -44,7 +44,7 @@ function SideBar() {
           <Menu.Item key="4" icon={<FileTextOutlined />}>
             Phiếu bảo trì
           </Menu.Item>
-          <Menu.Item key="4" icon={<MessageOutlined />}>
+          <Menu.Item key="5" icon={<MessageOutlined />}>
             Chat
           </Menu.Item>
         </Menu>
@@ -52,12 +52,6 @@ function SideBar() {
           {collapsed ? 'Expand' : 'Collapse'}
         </button>
       </div>
-      <div className={styles.content}>
-        {/* Nội dung khác của ứng dụng sẽ nằm trong ô container này */}
-        <h1>Welcome to the Dashboard</h1>
-        <p>This is where your main content goes.</p>
-      </div>
-    </div>
   );
 }
 
