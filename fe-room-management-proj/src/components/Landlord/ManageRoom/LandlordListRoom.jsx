b63aa95d10aa7061df572063af435b38b434cdd1
Duc20 <hongduc20062002@gmail.com>
2024-11-11 16:58:31 +0700

    List room by landlord

--- fe-room-management-proj/src/components/Landlord/ManageRoom/LandlordListRoom.jsx
+++ fe-room-management-proj/src/components/Landlord/ManageRoom/LandlordListRoom.jsx
@@ -11,8 +11,11 @@ const LandlordListRoom = () => {
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(true);
   const [statusFilter, setStatusFilter] = useState(""); // Trạng thái bộ lọc
-  const [districtFilter, setDistrictFilter] = useState(""); // Quận bộ lọc
-  const [provinceFilter, setProvinceFilter] = useState(""); // Thành phố bộ lọc
+  const [provinces, setProvinces] = useState([]);
+  const [province, setProvince] = useState("");
+  const [category, setCategory] = useState();
+  const [page, setPage] = useState(1);
+  const [categories, setCategories] = useState([]);
   const [titleFilter, setTitleFilter] = useState(""); // Tiêu đề bộ lọc
   const [isModalVisible, setIsModalVisible] = useState(false); // Modal cho chỉnh sửa phòng
   const [currentRoom, setCurrentRoom] = useState(null); // Phòng hiện tại đang được chỉnh sửa
@@ -25,9 +28,7 @@ const LandlordListRoom = () => {
         const res = await axios.get(`http://localhost:8000/api/room/by-landlord`, {
           params: {
             status: statusFilter,
-            district: districtFilter,
-            province: provinceFilter,
-            title: titleFilter, // Thêm các bộ lọc vào params API
+            province: province === "Tất cả tỉnh" ? "" : province,
           },
           withCredentials: true,
         });
@@ -38,25 +39,49 @@ const LandlordListRoom = () => {
         setLoading(false);
       }
     };
+    Promise.all([
+      axios.get("https://vapi.vnappmob.com/api/province/"),
+      axios.get("http://localhost:8000/api/room-category/all"),
+    ]).then(([resCities, resRoomCates]) => {
+      resCities.data.results.unshift({
+        province_name: "Tất cả tỉnh",
+        province_id: "",
+      });
+      setProvinces(
+        resCities.data.results.map((city) => ({
+          label: city.province_name,
+          value: city.province_id,
+        }))
+      );
+
+      resRoomCates.data.categories.unshift({
+        _id: 0,
+        category: "Tất cả loại phòng",
+      });
+      setCategories(
+        resRoomCates.data.categories.map((category) => ({
+          label: category.category,
+          value: category._id,
+        }))
+      );
+    });
 
     fetchRooms();
-  }, [statusFilter, districtFilter, provinceFilter, titleFilter]); // Gọi lại API khi bất kỳ bộ lọc nào thay đổi
+  }, [statusFilter, province, titleFilter]); // Gọi lại API khi bất kỳ bộ lọc nào thay đổi
 
   const handleStatusChange = (value) => {
     setStatusFilter(value); // Cập nhật trạng thái bộ lọc
   };
 
-  const handleDistrictChange = (value) => {
-    setDistrictFilter(value); // Cập nhật quận bộ lọc
-  };
+  const handleChangeProvince = async (value) => {
+    const selectedProvince = provinces.find(prov => prov.value === value);
+    setProvince(selectedProvince ? selectedProvince.label : "Tất cả tỉnh");
+};
 
-  const handleProvinceChange = (value) => {
-    setProvinceFilter(value); // Cập nhật thành phố bộ lọc
-  };
-
-  const handleTitleChange = (e) => {
-    setTitleFilter(e.target.value); // Cập nhật tiêu đề bộ lọc
-  };
+  const handleChangeCategory = (value) => {
+    const selectedCategory = categories.find(cat => cat.value === value);
+    setCategory(selectedCategory ? selectedCategory.label : "Tất cả loại phòng");
+};
 
   const handleEdit = (roomId) => {
     // Lấy thông tin phòng để chỉnh sửa
@@ -109,57 +134,8 @@ const LandlordListRoom = () => {
       title: "Tiêu đề",
       dataIndex: "title",
       key: "title",
-      filterDropdown: ({ setSelectedKeys, confirm }) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            autoFocus
-            placeholder="Lọc theo tiêu đề"
-            onChange={(e) => setTitleFilter(e.target.value)} // Cập nhật bộ lọc theo tiêu đề
-            value={titleFilter}
-            onPressEnter={() => confirm()}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Button
-            type="primary"
-            onClick={() => confirm()}
-            icon={<EditOutlined />}
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            Lọc
-          </Button>
-        </div>
-      ),
     },
-    {
-      title: "Quận/Huyện",
-      render: (text, record) => record.address.district,
-      filterDropdown: ({ setSelectedKeys, confirm }) => (
-        <div style={{ padding: 8 }}>
-          <Select
-            showSearch
-            placeholder="Lọc theo quận"
-            value={districtFilter}
-            onChange={handleDistrictChange}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          >
-            <Option value="">Tất cả</Option>
-            <Option value="district1">Quận 1</Option>
-            <Option value="district2">Quận 2</Option>
-            <Option value="district3">Quận 3</Option>
-          </Select>
-          <Button
-            type="primary"
-            onClick={() => confirm()}
-            icon={<EditOutlined />}
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            Lọc
-          </Button>
-        </div>
-      ),
-    },
+
     {
       title: "Tỉnh/Thành phố",
       render: (text, record) => record.address.province,
@@ -168,30 +144,29 @@ const LandlordListRoom = () => {
           <Select
             showSearch
             placeholder="Lọc theo thành phố"
-            value={provinceFilter}
-            onChange={handleProvinceChange}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
+            value={province}
+            onChange={handleChangeProvince}
+            style={{ width: 188, marginBottom: 8, display: "block" }}
           >
-            <Option value="">Tất cả</Option>
-            <Option value="Thành phó Hà Nội">Hà Nội</Option>
-            <Option value="Thành phố Hồ Chí Minh">Hồ Chí Minh</Option>
+            {provinces.map((loc) => (
+              <Select.Option key={loc.value} value={loc.value}>
+                {loc.label}
+              </Select.Option>
+            ))}
           </Select>
-          <Button
-            type="primary"
-            onClick={() => confirm()}
-            icon={<EditOutlined />}
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            Lọc
-          </Button>
         </div>
       ),
     },
     {
       title: "Trạng thái",
       render: (text, record) => (
-        <span>{record.status === "available" ? "Còn trống" : record.status === "rented" ? "Đã thuê" : "Đang bảo trì"}</span>
+        <span>
+          {record.status === "available"
+            ? "Còn trống"
+            : record.status === "rented"
+            ? "Đã thuê"
+            : "Đang bảo trì"}
+        </span>
       ),
       filterDropdown: ({ setSelectedKeys, confirm }) => (
         <div style={{ padding: 8 }}>
@@ -200,33 +175,24 @@ const LandlordListRoom = () => {
             placeholder="Lọc theo trạng thái"
             value={statusFilter}
             onChange={handleStatusChange}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
+            style={{ width: 188, marginBottom: 8, display: "block" }}
           >
             <Option value="">Tất cả</Option>
             <Option value="available">Còn trống</Option>
             <Option value="rented">Đã thuê</Option>
             <Option value="maintenance">Đang bảo trì</Option>
           </Select>
-          <Button
-            type="primary"
-            onClick={() => confirm()}
-            icon={<EditOutlined />}
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            Lọc
-          </Button>
         </div>
       ),
     },
     {
       title: "Sửa",
       render: (text, record) => (
-        <Button 
-          icon={<EditOutlined />} 
-          onClick={() => handleEdit(record._id)} 
-          type="primary" 
-          shape="circle" 
+        <Button
+          icon={<EditOutlined />}
+          onClick={() => handleEdit(record._id)}
+          type="primary"
+          shape="circle"
         />
       ),
     },
@@ -239,11 +205,7 @@ const LandlordListRoom = () => {
           okText="Có"
           cancelText="Không"
         >
-          <Button 
-            icon={<DeleteOutlined />} 
-            type="danger" 
-            shape="circle" 
-          />
+          <Button icon={<DeleteOutlined />} type="danger" shape="circle" />
         </Popconfirm>
       ),
     },
@@ -257,9 +219,15 @@ const LandlordListRoom = () => {
       <Table
         columns={columns}
         dataSource={rooms}
-        rowKey="_id"
         loading={loading}
-        pagination={{ pageSize: 5 }}
+        rowKey="_id"
+        pagination={{
+          pageSize: 10,
+          onChange: (page) => {
+            setPage(page);
+          },
+          total: rooms.length, // Replace with the actual total count if available
+        }}
       />
 
       <Modal
