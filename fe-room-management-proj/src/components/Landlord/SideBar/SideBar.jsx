7334a74b39bd85d74f0c76753e343a3fa632d2dc
Duc20 <hongduc20062002@gmail.com>
2024-11-11 17:25:33 +0700

    update sidebar

--- fe-room-management-proj/src/components/Landlord/SideBar/SideBar.jsx
+++ fe-room-management-proj/src/components/Landlord/SideBar/SideBar.jsx
@@ -1,12 +1,14 @@
 import {
   AppstoreOutlined,
+  ArrowLeftOutlined,
+  ArrowRightOutlined,
   DollarOutlined,
   FileTextOutlined,
   HomeOutlined,
   MessageOutlined,
   PieChartOutlined
 } from '@ant-design/icons';
-import { Menu } from 'antd';
+import { Button, Menu } from 'antd';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SideBar.module.css';
@@ -21,38 +23,44 @@ function SideBar({user}) {
   };
 
   return (
- 
-      <div className={`${styles.sidebar} ${collapsed ? styles.close : ''}`}>
-        <div className={styles.logoDetails}>
-          <AppstoreOutlined />
-          <span className={styles.logoName}>{user?.fullName}</span>
-        </div>
-        <Menu
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={collapsed}
-          className={styles.menu}
-        >
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            <Link to="/landlord/statistical">Thống kê</Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<HomeOutlined />}>
-            <Link to="/landlord/rooms">Phòng</Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<DollarOutlined />}>
-            Hóa đơn
-          </Menu.Item>
-          <Menu.Item key="4" icon={<FileTextOutlined />}>
-            Phiếu bảo trì
-          </Menu.Item>
-          <Menu.Item key="5" icon={<MessageOutlined />}>
-            Chat
-          </Menu.Item>
-        </Menu>
-        <button onClick={toggleCollapse} className={styles.collapseButton}>
-          {collapsed ? 'Expand' : 'Collapse'}
-        </button>
+    <div className={`${styles.sidebar} ${collapsed ? styles.close : ""}`}>
+      <div className={styles.logoDetails}>
+        {!collapsed && (
+          <>
+            <AppstoreOutlined />
+            <span className={styles.logoName}>{user?.fullName}</span>
+          </>
+        )}
       </div>
+      <Menu
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+        className={styles.menu}
+      >
+        <Menu.Item key="1" icon={<PieChartOutlined />}>
+          <Link to="/landlord/statistical">Thống kê</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<HomeOutlined />}>
+          <Link to="/landlord/rooms">Phòng</Link>
+        </Menu.Item>
+        <Menu.Item key="3" icon={<DollarOutlined />}>
+          Hóa đơn
+        </Menu.Item>
+        <Menu.Item key="4" icon={<FileTextOutlined />}>
+          Phiếu bảo trì
+        </Menu.Item>
+        <Menu.Item key="5" icon={<MessageOutlined />}>
+          Chat
+        </Menu.Item>
+      </Menu>
+      <Button
+        type="primary"
+        icon={collapsed ? <ArrowRightOutlined /> : <ArrowLeftOutlined />}
+        onClick={toggleCollapse}
+        className={styles.ocButton}
+      ></Button>
+    </div>
   );
 }
 
