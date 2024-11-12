cddf4404871b2a7102f1186a92937d623d6e8822
Duc20 <hongduc20062002@gmail.com>
2024-11-12 17:58:27 +0700

    edit room

---
+++ fe-room-management-proj/src/components/Landlord/UpdateRoom/ModalUpdateRoom.jsx
@@ -0,0 +1,547 @@
+import {
+  AreaChartOutlined,
+  FileTextOutlined,
+  TeamOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import {
+  Button,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  message,
+  Modal,
+  Row,
+  Select,
+  Upload,
+} from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styles from "./ModalUpdateRoom.module.css";
+
+function ModalUpdateRoom({
+  visible,
+  onCancel,
+  initialValues,
+  currentRoom,
+  setIsModalVisible,
+  setRooms,
+  rooms,
+}) {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [imageUrls, setImageUrls] = useState(null); // State to store the uploaded image URL
+  const [servicerooms, setServicerooms] = useState([{}]); // State to store service rooms
+  const [categories, setCategories] = useState([]);
+  const [provinces, setProvinces] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [wards, setWards] = useState([]);
+  const [province, setProvince] = useState("");
+  const [district, setDistrict] = useState("");
+  const [ward, setWard] = useState("");
+  const [category, setCategory] = useState();
+
+
+  const handleChangeProvince = async (value) => {
+    const selectedProvince = provinces.find((prov) => prov.value === value);
+    setProvince(selectedProvince ? selectedProvince.label : "");
+    setDistrict("");
+    setWard("");
+
+    // Lấy danh sách quận dựa trên tỉnh đã chọn
+    try {
+      const res = await axios.get(
+        `https://vapi.vnappmob.com/api/province/district/${value}`
+      );
+      setDistricts(
+        res.data.results.map((d) => ({
+          label: d.district_name,
+          value: d.district_id,
+        }))
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeDistrict = async (value) => {
+    const selectedDistrict = districts.find((d) => d.value === value);
+    setDistrict(selectedDistrict ? selectedDistrict.label : "");
+    setWard("");
+
+    // Lấy danh sách phường dựa trên quận đã chọn
+    try {
+      const res = await axios.get(
+        `https://vapi.vnappmob.com/api/province/ward/${value}`
+      );
+      setWards(
+        res.data.results.map((w) => ({ label: w.ward_name, value: w.ward_id }))
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeWard = async (value) => {
+    const selectedWard = wards.find((w) => w.value === value);
+    setWard(selectedWard ? selectedWard.label : "");
+  };
+
+  const handleChangeCategory = (value) => {
+    const selectedCategory = categories.find((cat) => cat.value === value);
+    setCategory(selectedCategory ? selectedCategory.label : "");
+  };
+  const onFinish = async (values) => {
+    setLoading(true);
+
+    // Tạo đối tượng address từ các trường đã chọn
+    const address = {
+      province: province, // Lấy giá trị tỉnh
+      district: district, // Lấy giá trị quận
+      ward: ward, // Lấy giá trị phường
+      detail: values.detail, // Lấy chi tiết địa chỉ từ form
+    };
+
+    const images = imageUrls;
+    console.log(images);
+    // Gửi dữ liệu phòng cùng với address
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/api/room/update/${currentRoom._id}`,
+        {
+          ...values,
+          address: address,
+          images: images, // Gửi địa chỉ dưới dạng đối tượng
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      message.success("Cập nhật phòng thành công.");
+      setIsModalVisible(false);
+      setRooms(
+        rooms.map((room) =>
+          room._id === currentRoom._id ? { ...room, ...values } : room
+        )
+      );
+    } catch (error) {
+      console.error(error);
+      message.error("Cập nhật phòng thất bại.");
+    }
+  };
+
+  const handleImageChange = async (options) => {
+    const { file, onSuccess, onError } = options;
+    console.log("File:", file); // Log chi tiết file
+
+    try {
+      const formData = new FormData();
+      formData.append("roomImages", file); // Thêm ảnh vào formData
+
+      console.log("Gửi ảnh lên server...");
+
+      // Gửi ảnh lên server
+      const response = await axios.post(
+        "http://localhost:8000/api/room/uploadImage",
+        formData,
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+
+      // Kiểm tra phản hồi từ server
+      if (response.data && response.data.roomImageUrl) {
+        console.log(
+          "Ảnh đã được tải lên thành công: ",
+          response.data.roomImageUrl
+        );
+
+        setImageUrls((prevUrls) => {
+          // Nếu prevUrls chưa được khởi tạo (null), khởi tạo nó là mảng rỗng
+          const updatedUrls = prevUrls
+            ? [...prevUrls, response.data.roomImageUrl]
+            : [response.data.roomImageUrl];
+          console.log("Cập nhật imageUrls:", updatedUrls);
+          return updatedUrls;
+        });
+
+        if (onSuccess) {
+          onSuccess("Upload thành công"); // Thông báo upload thành công
+        }
+      } else {
+        throw new Error("Không nhận được URL ảnh từ server");
+      }
+    } catch (error) {
+      console.error(
+        "Lỗi khi tải ảnh lên: ",
+        error.response ? error.response.data : error.message
+      );
+      if (onError) {
+        onError(error); // Gọi onError để xử lý lỗi
+      }
+    }
+  };
+
+  const handleRemove = (file) => {
+    setImageUrls(imageUrls.filter((url) => url !== file.url)); // Loại bỏ ảnh khỏi mảng khi xóa
+  };
+
+  const handleAddServiceRoom = () => {
+    setServicerooms([...servicerooms, {}]);
+  };
+
+  const handleRemoveServiceRoom = (index) => {
+    const newServicerooms = [...servicerooms];
+    newServicerooms.splice(index, 1);
+    setServicerooms(newServicerooms);
+  };
+
+  useEffect(() => {
+    // Lấy danh sách tỉnh và loại phòng
+    Promise.all([
+      axios.get("https://vapi.vnappmob.com/api/province/"),
+      axios.get("http://localhost:8000/api/room-category/all"),
+    ]).then(([resCities, resRoomCates]) => {
+      setProvinces(
+        resCities.data.results.map((city) => ({
+          label: city.province_name,
+          value: city.province_id,
+        }))
+      );
+      setCategories(
+        resRoomCates.data.categories.map((category) => ({
+          label: category.category,
+          value: category._id,
+        }))
+      );
+    });
+  }, []);
+  return (
+    <Modal
+      title="Cập nhật thông tin phòng"
+      visible={visible}
+      onCancel={onCancel}
+      footer={null} // Để tùy chỉnh nút bấm trong form
+      width={"80%"}
+      style={{ top: 0, height: '100vh' }} // Đặt modal bắt đầu từ đầu trang và cao 100% viewport
+      bodyStyle={{ height: 'calc(100% - 55px)', overflowY: 'auto' }} // Đảm bảo nội dung cuộn khi cần
+    >
+      <Form
+        form={form}
+        initialValues={initialValues}
+        layout="vertical"
+        onFinish={onFinish}
+      >
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Tiêu đề phòng"
+              name="title"
+              rules={[
+                { required: true, message: "Vui lòng nhập tiêu đề phòng!" },
+              ]}
+            >
+              <Input
+                placeholder="Nhập tiêu đề phòng"
+                prefix={<FileTextOutlined />}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Loại phòng"
+              name="category"
+              rules={[{ required: true, message: "Vui lòng chọn loại phòng!" }]}
+            >
+              <Select
+                placeholder="Chọn loại phòng"
+                onChange={handleChangeCategory}
+              >
+                {categories.map((cat) => (
+                  <Select.Option key={cat.value} value={cat.value}>
+                    {cat.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        {/* Địa chỉ */}
+        <Row gutter={16}>
+          <Col span={6}>
+            <Form.Item
+              label="Tỉnh"
+              name="province"
+              rules={[{ required: true, message: "Vui lòng chọn tỉnh!" }]}
+            >
+              <Select
+                placeholder="Chọn tỉnh/thành phố"
+                onChange={handleChangeProvince}
+              >
+                {provinces.map((loc) => (
+                  <Select.Option key={loc.value} value={loc.value}>
+                    {loc.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Quận"
+              name="district"
+              rules={[{ required: true, message: "Vui lòng chọn quận!" }]}
+            >
+              <Select
+                placeholder="Chọn quận/huyện"
+                onChange={handleChangeDistrict}
+              >
+                {districts.map((d) => (
+                  <Select.Option key={d.value} value={d.value}>
+                    {d.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Phường"
+              name="ward"
+              rules={[{ required: true, message: "Vui lòng chọn phường!" }]}
+            >
+              <Select placeholder="Chọn xã/phường" onChange={handleChangeWard}>
+                {wards.map((w) => (
+                  <Select.Option key={w.value} value={w.value}>
+                    {w.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              name="detail"
+              label="Địa chỉ"
+              rules={[{ required: true, message: "Vui lòng nhập địa chỉ" }]}
+            >
+              <Input placeholder="Địa chỉ" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={8}>
+            <Form.Item
+              label="Diện tích (m²)"
+              name="acreage"
+              rules={[
+                { required: true, message: "Vui lòng nhập diện tích phòng!" },
+              ]}
+            >
+              <InputNumber
+                min={1}
+                placeholder="Nhập diện tích"
+                prefix={<AreaChartOutlined />}
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              label="Giá phòng (VND)"
+              name="price"
+              rules={[{ required: true, message: "Vui lòng nhập giá phòng!" }]}
+            >
+              <InputNumber
+                min={1}
+                placeholder="Nhập giá phòng"
+                prefix="VNĐ"
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              label="Số người tối đa"
+              name="maxSize"
+              rules={[
+                { required: true, message: "Vui lòng nhập số người tối đa!" },
+              ]}
+            >
+              <InputNumber
+                min={1}
+                placeholder="Nhập số người tối đa"
+                prefix={<TeamOutlined />}
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              label="Mô tả phòng"
+              name="description"
+              rules={[
+                { required: true, message: "Vui lòng nhập mô tả phòng!" },
+              ]}
+            >
+              <Input.TextArea placeholder="Mô tả phòng" rows={4} />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        {/* Dịch vụ phòng */}
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item label="Dịch vụ phòng" name="servicerooms">
+              {servicerooms.map((_, index) => (
+                <Row gutter={16} key={index} className="servicerooms">
+                  <Col span={6}>
+                    <Form.Item
+                      name={["servicerooms", index, "name"]}
+                      label="Tên dịch vụ"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Vui lòng nhập tên dịch vụ!",
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Tên dịch vụ" />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Form.Item
+                      name={["servicerooms", index, "price"]}
+                      label="Giá dịch vụ"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Vui lòng nhập giá dịch vụ!",
+                        },
+                      ]}
+                    >
+                      <InputNumber
+                        min={1}
+                        placeholder="Giá dịch vụ"
+                        style={{ width: "100%" }}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Form.Item
+                      name={["servicerooms", index, "description"]}
+                      label="Mô tả dịch vụ"
+                    >
+                      <Input.TextArea placeholder="Mô tả dịch vụ" />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Button
+                      type="danger"
+                      onClick={() => handleRemoveServiceRoom(index)}
+                      className={styles.removeServiceButton}
+                    >
+                      Xóa
+                    </Button>
+                  </Col>
+                </Row>
+              ))}
+              <Button type="dashed" onClick={handleAddServiceRoom}>
+                Thêm dịch vụ phòng
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        {/* Tiện nghi phòng */}
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item label="Tiện nghi phòng" name="amenities">
+              <Checkbox.Group>
+                <Row>
+                  <Col span={6}>
+                    <Checkbox value="WiFi">Wi-Fi</Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Điều hòa">Điều hòa</Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Bãi đỗ xe thoáng rộng">Bãi đỗ xe</Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Giờ giấc tự do">Giờ giấc tự do</Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Bảo vệ 24/7">Bảo vệ 24/7</Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Full nội thất hiện đại, sang trọng">
+                      Nội thất
+                    </Checkbox>
+                  </Col>
+                  <Col span={6}>
+                    <Checkbox value="Gần chợ, địa điểm ăn uống...">
+                      Gần chợ
+                    </Checkbox>
+                  </Col>
+
+                  <Col span={6}>
+                    <Checkbox value="Có tháng máy">Thang máy</Checkbox>
+                  </Col>
+                </Row>
+              </Checkbox.Group>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="Hình ảnh phòng" name="images">
+              <Upload
+                name="roomImages"
+                listType="picture-card"
+                multiple={true} // Cho phép chọn nhiều ảnh
+                showUploadList={{ showRemoveIcon: true }} // Hiển thị nút xóa
+                customRequest={handleImageChange} // Đảm bảo customRequest được truyền đúng
+                // onChange={handleImageChange}
+                onRemove={handleRemove} // Xử lý khi xóa ảnh
+              >
+                {imageUrls && imageUrls.length > 0
+                  ? imageUrls.map((url, index) => (
+                      <img
+                        key={index}
+                        src={`http://localhost:8000${url}`}
+                        alt={`room-${index}`}
+                        style={{ width: "100%" }}
+                      />
+                    ))
+                  : null}
+                <div>
+                  <UploadOutlined />
+                  <div style={{ marginTop: 8 }}>Chọn hình ảnh</div>
+                </div>
+              </Upload>
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Cập nhật
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
+
+export default ModalUpdateRoom;
